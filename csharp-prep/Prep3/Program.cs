5d834f3f589a47edcb94944737bbd090b613db4e
cagedjoos <joos.cage@gmail.com>
2024-09-20 09:19:29 -0600

    Modified the file to complete the assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicnumber = randomGenerator.Next(1, 101);
+        int guessnumber = -1;
+        while (guessnumber != magicnumber)
+        {
+            Console.Write("What is your guess?");
+            guessnumber = int. Parse(Console.ReadLine());
+
+
+            if (guessnumber > magicnumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guessnumber < magicnumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed It!");
+            }
+        }
     }
 }
