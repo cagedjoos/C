136d0d052fe4d7e2b28099fd38fd858b04154cf5
cagedjoos <joos.cage@gmail.com>
2024-09-20 09:42:57 -0600

    Modified the file to complete the assignment

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int number = -1;
+        
+        while (number != 0)
+        {
+            Console.Write("Enter a number (If you want to quit enter 0): ");
+            string numberAnswer = Console.ReadLine();
+            number = int.Parse(numberAnswer);
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+
+        int total = 0;
+
+        foreach (int numberOne in numbers)
+        {
+            total += numberOne;
+        }
+
+        Console.WriteLine($"The total of all of the numbers is {total}.");
+
+        float average = ((float)total) / numbers.Count;
+        Console.WriteLine($"The average of all of the numbers is {average}.");
+
+        int largeNum = numbers[0];
+
+        foreach (int num in numbers)
+        {
+            if (num > largeNum)
+            {
+                largeNum = num;
+            }
+        }
+
+        Console.WriteLine($"The largest number is {largeNum}");
     }
 }
