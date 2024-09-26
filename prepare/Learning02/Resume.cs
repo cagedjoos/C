c2c3fb68810edcb804176c1d4ac853ce3887364a
cagedjoos <joos.cage@gmail.com>
2024-09-26 10:16:15 -0600

    Modified the file to work on the assignment

--- prepare/Learning02/Resume.cs
+++ prepare/Learning02/Resume.cs
@@ -6,7 +6,6 @@ public class Resume
     {
         Console.WriteLine($"Name: {_name}");
         Console.WriteLine("Jobs:");
-
         foreach (Job job in _jobs)
         {
             job.Display();
